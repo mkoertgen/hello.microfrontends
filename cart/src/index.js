c92a8dfe8e5696b38f867b94a933890b4a05dfef
Marcel Körtgen <marcel.koertgen@gmail.com>
2021-01-03 18:04:48 +0100

    Integrate cart into container

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,5 @@
-import faker from 'faker';
+import faker from "faker";
 
 const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
-document.querySelector('#cart-dev').innerHTML = cartText;
+document.querySelector("#dev-cart").innerHTML = cartText;
